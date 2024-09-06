0c8bc0f1b37ab2661744b2d6631ec63fa7953a30
Ambs04 <amberleejones2007@gmail.com>
2024-09-06 19:34:21 +0200

    final commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -10,31 +10,39 @@
 
 // console.log(sum)
 
+let player = {
+    name: "Player",
+    tokens: 200
+}
+
 //SHOWING FOR START OF GAME
-let gotBlackJack = false
-let stillPlaying = false
 let cards= []
 let sum = 0
-
+let status = ""
+let gotBlackJack = false
+let stillPlaying = false
 
 //DECLARATIONS
 let playerStatus = document.getElementById("player-status")
 let cardsSel = document.getElementById("cards-sel")
 let sumSel = document.getElementById("sum-sel")
-
+let playerSel = document.getElementById("player-sel")
+playerSel.textContent = player.name + ": R" + player.tokens 
 
 //FUNCTIONS
 
 function randomCard() {
-    let randomNum = Math.floor( Math.random()*11 ) +2
+    let randomNum = Math.floor( Math.random()*11 ) + 2
+    return randomNum
 }
 
 
 function startGame() {
-    let cardOne = randomCard();
-    let cardTwo = randomCard();
-    cards = [cardOne, cardTwo];
-    sumCards = cardOne + cardTwo;
+    let cardOne = randomCard()
+    let cardTwo = randomCard()
+    cards = [cardOne, cardTwo,]
+    sumCards = cardOne + cardTwo 
+    stillPlaying = true
     loadGame()
 }
 
@@ -42,32 +50,35 @@ function loadGame() {
     //load for cards
     cardsSel.textContent = "Cards: "
     //keep displaying loaded cards
-    for (let i = 0; i<cards.length; i++) {
-        cardsSel.textContent = cardsSel.textContent + cards[i] + " "
-    };
+    for (let i = 0; i < cards.length; i++) {
+        cardsSel.textContent += cards[i] + " "
+    }
     //load for sum
-    sumSel.textContent = "Sum: " + sumCards;
+    sumSel.textContent = "Sum: " + sumCards
     
 //BOOLEANS
 
-    if (sum <= 20) {
-        playerStatus = "Would you like to draw another card? :)"
-    } else if (sum === 21){
-        playerStatus = "BlackJack!!! XD"
+    if (sumCards <= 20) {
+        status = "Would you like to draw another card? :)"
+        stillPlaying = true
+    } else if (sumCards === 21) {
+        status = "BlackJack!!! XD"
         gotBlackJack = true
-    } else {
-        playerStatus = "You lose. :("
+        
+    } else if (sumCards > 21) {
+        status = "You lose. :("
         stillPlaying = false
+    
     }
 
-        playerStatus.textContent = playerStatus
+        playerStatus.textContent = status
 }
 
 function drawNewCard() {
-    if (gotBlackJack===false && stillPlaying===true ) {
-        let cardThree = randomNum()
-        sum = sum + cardThree
-        carThree.push(cards)
+    if (gotBlackJack===false && stillPlaying===true) {
+        let cardThree = randomCard()
+        sumCards += cardThree
+        cards.push(cardThree)
         loadGame()
     }
 }
